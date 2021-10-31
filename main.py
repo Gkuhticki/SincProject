4fbe6717ded94918732d07401e31482866d30421
Gkuhticki <72451397+Gkuhticki@users.noreply.github.com>
2021-10-31 15:28:32 +0300

    Update main.py
    Проверил несколько раз все работает как часы. Добавил еще несколько функций работающих до этого отдельно добавил возможность выбора каталогов исправил другие мелкие баги. Думаю проект готов.

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ import time
 from datetime import datetime
 
 
-# Каталоги и деревья каталогов.
-src = "/home/georg/my_projects/Test/src/"
-dst = "/home/georg/my_projects/Test/dst/"
-
 log = []
-len_src = len(src)
-len_dst = len(dst)
+
+
 
 # каталоги с root взял на всякий случай
 # каталоги без root нужны для успешного создания каталогов в dst, и для сравнения итоговых данных
@@ -28,6 +24,7 @@ path_time_file_dst = []
 
 def scraping_files_catalogs_of_src():
     tree_src = os.walk(src, topdown=True)
+    len_src = len(src)
     for (root, dirs, files) in tree_src:
         inner_tempfiles = []
         inner_temp_path = []
@@ -72,6 +69,7 @@ def scraping_files_catalogs_of_src():
 
 
 def scraping_files_catalogs_of_dst():
+    len_dst = len(dst)
     tree_dst = os.walk(dst, topdown=True)
     for (root, dirs, files) in tree_dst:
         inner_tempfiles = []
@@ -147,7 +145,6 @@ def compare_changing():
 
 def copy_files():
     for dirs in time_file_only_in_src:
-        print (dirs)
         dir_filesrc = str(src + dirs[0] + dirs[2])
         pathdst = str(dst + dirs[0])
         try:
@@ -245,6 +242,39 @@ def per_sinc():
             print("Это не верное значение введите предложанные данные")
             continue
 
+def choice_catalogs():
+    p1 = """
+    Пожалуйста введите полный путь до корневого каталога откуда выгружать данные 
+    по следующему шаблону: /home/folder/еще папка/конечный каталог/
+    """
+    p2 = """
+        Пожалуйста введите полный путь до корневого каталога куда выгружать данные 
+        по следующему шаблону: /home/folder/еще папка/конечный каталог/
+        """
+    p3 = """
+            Пожалуйста введите полный путь до корневого каталога куда сохранять файл логов 
+            по следующему шаблону: /home/folder/еще папка/конечный каталог/
+            """
+    print(p1)
+    path_src = input()
+    print(p2)
+    path_dst = input()
+    print(p3)
+    path_log_int = input()
+    global src
+    global dst
+    global path_log
+    src = path_src
+    dst = path_dst
+    path_log = path_log_int
+
+
+def logging():
+    log_p_f = path_log + 'logFile.txt'
+    f = open( log_p_f, 'a' )
+    for item in log: f.write("%s\n" % item)
+    f.close()
+
 
 def main_func():
     i = 0
@@ -263,10 +293,7 @@ def main_func():
         remove_catalogs()
         copy_files()
 
-
-
-        for l in log:
-            print(l)
+        logging()
         # Отчищаем списки перед следующим циклом
         clear_all_list()
         i += 1
@@ -277,6 +304,7 @@ def main_func():
 
         time.sleep(lot_time)
 
-        
+
 per_sinc()
+choice_catalogs()
 main_func()
